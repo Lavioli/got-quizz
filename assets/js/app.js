cdca1907812444c9e094afe399148357381d3841
runnerjustin63 <justinpbarnes1@gmail.com>
2016-08-25 17:01:27 -0500

    add jquery and html wireframe

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() 
+{
+$('.starting').css('display', 'block');
+event.preventDefault();	
+$('.starting').click(function(){
+	$('.starting').css('display', 'none');
+	//$('.starting').hide();
+//alert("is this working ffs?")
+
+
+})
+
+
+
+
+
+});
+
+
+
+
+//var questions {
+
+
+
+
+
+
+
+//}
